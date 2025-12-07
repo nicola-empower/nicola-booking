3a0c52368a1cea9651956af8e96fe2605ed4d301
Nicola Berry <nicola@empowervaservices.co.uk>
2025-12-07 19:35:08 +0000

    fix(deps): Downgrade date-fns and fix locale import

--- app/components/AdminCalendar.tsx
+++ app/components/AdminCalendar.tsx
@@ -3,7 +3,7 @@
 import React, { useState, useEffect } from "react";
 import { Calendar, dateFnsLocalizer, Views } from "react-big-calendar";
 import { format, parse, startOfWeek, getDay, addMinutes } from "date-fns";
-import { enUS } from "date-fns/locale/en-US";
+import { enUS } from "date-fns/locale";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import { collection, onSnapshot, query, orderBy, Timestamp } from "firebase/firestore";
 import { db } from "../utils/firebase";
